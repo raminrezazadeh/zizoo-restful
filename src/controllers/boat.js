7af1010b84e9287fb6c4c67b2f90af07d7dddccf
Ramin Rezazadeh <ramin.rezazadeh@mobiliva.com>
2020-01-18 10:51:37 +0300

    Create all routes and controllers - mock

--- src/controllers/boat.js
+++ src/controllers/boat.js
@@ -1,3 +1,19 @@
 module.exports.list = async (request, response) => {
-  return response.json({test: 66777});
+  return response.json({test: 'list'});
+};
+
+module.exports.find = async (request, response) => {
+  return response.json({test: 'find'});
+};
+
+module.exports.update = async (request, response) => {
+  return response.json({test: 'update'});
+};
+
+module.exports.remove = async (request, response) => {
+  return response.json({test: 'remove'});
+};
+
+module.exports.create = async (request, response) => {
+  return response.json({test: 'create'});
 };
--- src/routes/boat.js
+++ src/routes/boat.js
@@ -3,5 +3,9 @@ const BoatController = require('../controllers/boat');
 const router = express.Router();
 
 router.get('/', BoatController.list);
+router.get('/:id', BoatController.find);
+router.put('/:id', BoatController.update);
+router.delete('/:id', BoatController.remove);
+router.post('/', BoatController.create);
 
 module.exports = router;
